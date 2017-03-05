152ac587cbb5d5fa40ac0c65da8c3a2718f1e0f9
ywb1005 <1058133702@qq.com>
2017-03-05 22:03:05 +0800

    add router and vuex

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import router from './router'
+import store from './vuex'
+import { sync } from 'vuex-router-sync'
+
+sync(store, router)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+const app = new Vue({
+  store,
+  router,
+  ...App
 })
+app.$mount('#app')
