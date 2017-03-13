1eb4a43ea05b919eb0de93dd71edaa8e25be19db
ywb1005 <1058133702@qq.com>
2017-03-13 10:11:43 +0800

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="header">
-    <h4>ddd</h4>
+  	<div class="img">
+  		<img src="../assets/index.png" />
+  	</div>
+    <div class="search">
+    	<el-form ref="form" :model="form" label-width="90px">
+    		<el-form-item label="搜索本站">
+    			<el-input v-model="input" placeholder="请输入内容" icon="search"></el-input>
+    		</el-form-item>
+    	</el-form>
+    </div>
+    <div class="user">
+    	<span class="spn">注册</span>
+    	<img style="width: 20px;height: 25px;" src="../assets/line.png"/>
+    	<span class="spn">登录</span>
+    </div>
+    <div class="menu">
+   <el-dropdown>
+  <span class="el-dropdown-link">
+  <i class="el-icon-menu"></i>
+  </span>
+  <el-dropdown-menu slot="dropdown">
+    <el-dropdown-item>注册</el-dropdown-item>
+    <el-dropdown-item>登录</el-dropdown-item>
+  </el-dropdown-menu>
+</el-dropdown>
+    </div>
   </div>
 </template>
 <style>
@@ -8,8 +33,65 @@
     position: fixed;
     top:0px;
     left: 0px;
-    background-color:red;
+    background-color:#EEEEEE;
     width: 100%;
     height: 10%;
+  }
+  .img{
+  	width: 10%;
+  	height: 80px;
+  	float: left;
+  	margin-left:15% ;
+  }
+  .search{
+  	margin-left: 30%;
+  	float: left;
+  	width: 20%;
+  	height: 80px;
+  	padding-top: 30px;
+  }
+  .spn:hover{
+  	color: #20A0FF;
+  	text-decoration:underline
+  }
+  @media (min-width: 1248px){
+  	.menu{
+  		display: none;
+  	}
+	.user{
+	  	width: 10%;
+	  	height: 80px;
+	  	margin-left: 2%;
+	  	float: left;
+	  	font-size: 22px;
+	  	padding-top: 30px;
+  	}
+  }
+  @media (min-width: 900px) and (max-width:1248px){
+	.user{
+	  	display: none;
+  	}
+  	.menu{
+  		width: 10%;
+	  	height: 80px;
+	  	margin-left: 2%;
+	  	float: left;
+	  	padding-top: 40px;
+  	}
+  }
+  @media (max-width: 900px){
+  	.search{
+  		display: none;
+  	}
+  	.user{
+	  	display: none;
+  	}
+  	.menu{
+  		width: 10%;
+	  	height: 80px;
+	  	margin-left: 30%;
+	  	float: left;
+	  	padding-top: 40px;
+  	}
   }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,21 +1,29 @@
 <template>
-  <!--<div class="header">-->
-    <!--<h1>ddd</h1>-->
-  <!--</div>-->
-  <div>
-    <header></header>
+  <div class="container">
+  		<headers></headers>
+    <div class="row">
+    <h1>asss</h1>
+    </div>
   </div>
 </template>
 <script>
-  import header from '../components/header.vue'
+  import headers from 'components/header.vue'
   export default {
     components: {
-      header
+      headers
     }
   }
 </script>
 <style>
-  .bg-purple {
-    background: #d3dce6;
-  }
+.container{
+	width: 100%;
+	height: 100%;
+}
+.row{
+  position: relative;
+  top: 22px;
+  width: 90%;
+ 	height: 100%;
+	background-color: #20A0FF;
+ }
 </style>
